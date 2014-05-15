c180ac62a4f9c2a341c4b26dcdf45608a89c301d
TH <tayhalla@gmail.com>
2014-05-14 18:29:26 -0700

    ironing out the response format

--- test/sneakers.test.js
+++ test/sneakers.test.js
@@ -5,7 +5,7 @@ AmazonRequest 	= AmazonSneakers(ENV['AWS_KEY'], ENV['AWS_PASS'], ENV['AWS_ASSOC_
 
 describe('Making a request to Amazon', function () {
 	it('should return a list of shoes', function (done) {
-		AmazonRequest.query('Nike', { brand : 'Nike', page : 1, gender : 'male'}, function(err, res){			
+		AmazonRequest.query('Nike', { brand : 'Brooks', page : 1, gender : 'male'}, function(err, res){						
 			console.log(res);
 			done(err, res);			
 		});		
