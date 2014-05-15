0bcacc552f44830377c62a31250bbb618c37f5bc
TH <tayhalla@gmail.com>
2014-05-15 11:09:05 -0700

    finished test suite, v 0.1 ready

--- test/sneakers.test.js
+++ test/sneakers.test.js
@@ -1,13 +1,102 @@
 var
+should					= require('should'),
+_ 							= require('underscore'),
 ENV 						= require('./../.env'),
 AmazonSneakers 	= require('./../lib/AmazonSneakers'),
-AmazonRequest 	= AmazonSneakers(ENV['AWS_KEY'], ENV['AWS_PASS'], ENV['AWS_ASSOC_KEY']);
+AmazonRequest 	= AmazonSneakers(ENV['AWS_KEY'], ENV['AWS_PASS']);
 
-describe('Making a request to Amazon', function () {
-	it('should return a list of shoes', function (done) {
-		AmazonRequest.query('Nike', { brand : 'Brooks', page : 1, gender : 'male'}, function(err, res){						
-			console.log(res);
-			done(err, res);			
+describe('Amazon Sneakers Test Suite', function () {
+	// Set Longer Timeout for api calls
+	this.timeout(6000);
+
+	describe('Making a request to Amazon with only a query string', function () {	
+		it('should return a list of shoes', function (done) {
+			AmazonRequest.query('nike', function(err, results){						
+				_.isArray(results).should.be.ok;
+				done(err, results);			
+			});		
+		});
+
+		it('should return an empty array for a empty string query', function (done) {
+			AmazonRequest.query('', function(err, results){												
+				_.isArray(results).should.be.ok;			
+				results.length.should.equal(0);			
+				done(err, results);			
+			});		
+		});
+
+		it('should return 10 entries for a generic text search', function (done) {
+			AmazonRequest.query('brooks', function(err, results){														
+				_.isArray(results).should.be.ok;
+				results.length.should.equal(10);			
+				done(err, results);
+			});		
+		});
+
+		it('should return different results for different text searches', function (done) {
+			// First Request
+			AmazonRequest.query('brooks', function(err, results){														
+				_.isArray(results).should.be.ok;
+				results.length.should.equal(10);
+				var previousTitle = _.last(results).Title;
+				previousTitle.should.be.type('string');
+				// Next Request
+				AmazonRequest.query('Nike', function(err, results){														
+					_.isArray(results).should.be.ok;
+					results.length.should.equal(10);
+					var nextTitle = _.last(results).Title;
+					nextTitle.should.be.type('string');
+					nextTitle.should.not.equal(previousTitle);
+					done(err, results);
+				});							
+			});		
 		});		
 	});
+
+	describe('Making a request with the query string + options', function () {
+		it('should allow for a brand selection', function (done) {
+			AmazonRequest.query('brooks', {brand: 'brooks'}, function(err, results){														
+				_.isArray(results).should.be.ok;
+				results.length.should.equal(10);			
+				_.last(results).Brand.should.equal('Brooks');
+				done(err, results);
+			});		
+		});
+
+		it('should allow for a gender selection', function (done) {
+			AmazonRequest.query('brooks', {gender: 'womens'}, function(err, results){																		
+				_.isArray(results).should.be.ok;
+				results.length.should.equal(10);											
+				var lowCaseResult = _.last(results).Title.toLowerCase().contains('women\'s').should.be.ok;				
+				done(err, results);
+			});		
+		});
+
+		it('should allow for a gender selection', function (done) {
+			AmazonRequest.query('brooks', function(err, results){														
+				_.isArray(results).should.be.ok;
+				results.length.should.equal(10);
+				var previousTitle = _.last(results).Title;
+				previousTitle.should.be.type('string');
+				// Next Request
+				AmazonRequest.query('brooks', { page: 2 }, function(err, results){														
+					_.isArray(results).should.be.ok;
+					results.length.should.equal(10);
+					var nextTitle = _.last(results).Title;
+					nextTitle.should.be.type('string');
+					nextTitle.should.not.equal(previousTitle);
+					done(err, results);
+				});							
+			});		
+		});
+
+	});
 });
+
+// String Prototype Additions
+if (!String.prototype.contains ) {
+  String.prototype.contains = function() {
+    return String.prototype.indexOf.apply( this, arguments ) !== -1;
+  };
+}
+
